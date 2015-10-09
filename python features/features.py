e99e56ca9ddb583b60b53aec09bbd86e266c953a
zenayte <sarashimekaw@gmail.com>
2015-10-09 10:13:35 +0200

    feature extraction

--- python features/features.py
+++ python features/features.py
@@ -1,50 +1,63 @@
 import cv2
-# im_blue = imread('..\..\sydney\ortho_blue\0_0_0_tex.tif');
-# im_red = imread('..\..\sydney\ortho_red\0_0_0_tex.tif');
-# im_green = imread('..\..\sydney\ortho_green\0_0_0_tex.tif');
-# im_nir = imread('..\..\sydney\ortho_nir\0_0_0_tex.tif');
+import numpy as np
+import scipy
+import skimage
+import scipy.ndimage.filters as sf
 
-im_blue = cv2.imread("..\..\sydney\ortho_blue\0_0_0_tex.tif")
-im_red = cv2.imread("..\..\sydney\ortho_red\0_0_0_tex.tif")
-im_green = cv2.imread("..\..\sydney\ortho_green\0_0_0_tex.tif")
-im_nir = cv2.imread("..\..\sydney\ortho_nir\0_0_0_tex.tif")
+# Import the images
+im_blue = cv2.imread("../../sydney/ortho_blue/0_0_0_tex.tif")
+im_red = cv2.imread("../../sydney/ortho_red/0_0_0_tex.tif")
+im_green = cv2.imread("../../sydney/ortho_green/0_0_0_tex.tif")
+im_nir = cv2.imread("../../sydney/ortho_nir/0_0_0_tex.tif")
 
-# im = im_red.*0.2989;
-# im(:,:,2) = im_green(:,:,2).*0.5870;
-# im(:,:,3) = im_blue(:,:,3).*0.1140;
+# Create a RGB image 
+redArray=np.array(im_red)
+im = np.multiply(redArray, 0.2989)
+greenSlice = im_green[:,:,1]
+im[:,:,1] = np.multiply(np.array(greenSlice),0.5870)
+blueSlice = im_blue[:,:,2]
+im[:,:,2] = np.multiply(np.array(blueSlice),0.1140)
 
-# impart = im(4000:4500, 2000:2500, :);
+# Make the image to an uint8
+#imUint8 = np.array(im,dtype=np.uint8)
 
-# inImage = imgaussfilt(impart, 8);
-# inImSize = size(inImage);
+# Divid the image
+impart = im[4000:4500, 2000:2500, :]
 
-# N=4; %NxN imSize
+# Filtrating the image
+inImage = impart
+inImage[:,:,0] = sf.gaussian_filter(impart[:,:,0], 8.0)
+inImage[:,:,1] = sf.gaussian_filter(impart[:,:,1], 8.0)
+inImage[:,:,2] = sf.gaussian_filter(impart[:,:,2], 8.0)
 
-# upperLimits = [floor(inImSize(1)/N)*N, floor(inImSize(2)/N)*N];
-# image = inImage(1:upperLimits(1),1:upperLimits(2));
+# Variables 
+inImSize = np.shape(inImage)
+N = 4
+upperLimits = np.array([np.floor(inImSize[0]/N)*N, np.floor(inImSize[1]/N)*N])
+image = inImage[1:upperLimits[0],1:upperLimits[1],:]
 # imSize = size(image);
 
-# newImage = zeros(imSize(1)/N-1, imSize(2)/N-1, 4);
+# An empty image
+newImage = np.zeros(upperLimits[0]/N-1, upperLimits[1]/N-1, 4);
 
-# for i=1:N:imSize(1)-N
-    # for k=1:N:imSize(2)-N
-        # extImage = image(i:i+(N-1), k:k+(N-1));
-        # stats = imStats(extImage);
+# Creating features contrast, energy, correlation, homogeneity
+for i in xrange (0,upperLimits[0]-N, N)
+	for k in xrange (0, upperLimits[1]-N, N)
+		extImage = image[i:i+(N-1), k:k+(N-1)]
+        stats = imStats(extImage)
         
-        # newImage(((i-1)/N)+1, ((k-1)/N)+1, 1) = stats.Contrast;
-        # newImage(((i-1)/N)+1, ((k-1)/N)+1, 2) = stats.Correlation;
-        # newImage(((i-1)/N)+1, ((k-1)/N)+1, 3) = stats.Energy;
-        # newImage(((i-1)/N)+1, ((k-1)/N)+1, 4) = stats.Homogeneity;
-        
-        
-    # end
-    # counter = i/(imSize(1)-N)
-# end
+        newImage[((i-1)/N)+1, ((k-1)/N)+1, 1] = stats.Contrast;
+        newImage[((i-1)/N)+1, ((k-1)/N)+1, 2] = stats.Correlation;
+        newImage[((i-1)/N)+1, ((k-1)/N)+1, 3] = stats.Energy;
+        newImage[((i-1)/N)+1, ((k-1)/N)+1, 4] = stats.Homogeneity;
 
-# newImage(:,:,1) = newImage(:,:,1)/max(max(newImage(:,:,1)));
-# newImage(:,:,2) = newImage(:,:,2)/max(max(newImage(:,:,2)));
-# newImage(:,:,3) = newImage(:,:,3)/max(max(newImage(:,:,3)));
-# newImage(:,:,4) = newImage(:,:,4)/max(max(newImage(:,:,4)));
+    print i/(upperLimits[0]-N)
+
+# Normalization
+newImage[:,:,1] = newImage[:,:,1]/np.amax(newImage[:,:,1]);
+newImage[:,:,2] = newImage[:,:,2]/np.amax(newImage[:,:,2]);
+newImage[:,:,3] = newImage[:,:,3]/np.amax(newImage[:,:,3]);
+newImage[:,:,4] = newImage[:,:,4]/np.amax(newImage[:,:,4]);
 
 print "Hello world"
 
