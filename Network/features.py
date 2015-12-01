7eea8a373c1d9fc00938cb981aabb667714a54c6
patto763 <patto763>
2015-12-01 08:41:09 +0100

    ny feature fil

---
+++ Network/features.py
@@ -0,0 +1,140 @@
+import cv2
+import numpy as np
+import scipy
+import skimage
+import scipy.ndimage.filters as sf
+from skimage.feature import greycomatrix, greycoprops
+from PIL import Image
+import utils
+import heightFeatures
+global levels, N, gaussNr
+
+np.set_printoptions(threshold = np.nan)
+
+def imStats(im_array,levels):
+	array_size=np.shape(im_array)[2]
+
+	measures = ['homogeneity', 'energy','dissimilarity','ASM']
+	greyco_array=np.zeros((len(measures),array_size))
+
+	for i in range(0,array_size):
+
+		image_block=im_array[:,:,i]
+		pixel_pairs = greycomatrix(image_block, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4 ], levels, normed = False)
+		pixel_pairs = np.array(pixel_pairs,dtype=np.float)
+		numberOPP = [np.sum(pixel_pairs[:,:,:,0]), np.sum(pixel_pairs[:,:,:,1]), np.sum(pixel_pairs[:,:,:,2]), np.sum(pixel_pairs[:,:,:,3])]
+
+		weightingOPP = np.zeros((levels,levels,1 ,1))
+		weightingOPP[:,:,:,0] = pixel_pairs[:,:,:,0]/numberOPP[0] + pixel_pairs[:,:,:,1]/numberOPP[1] + pixel_pairs[:,:,:,2]/numberOPP[2] + pixel_pairs[:,:,:,3]/numberOPP[3]
+
+		feature_arrays = np.zeros((len(measures)))
+		for index,measure in enumerate(measures):
+			feature_arrays[index] = greycoprops(weightingOPP,measure)
+
+		greyco_array[:,i]=feature_arrays
+		
+		print float(i)/float(array_size)
+
+	for j in range(0,array_size):
+		greyco_array[:,j]/=np.amax(greyco_array[:,j])
+	
+	return greyco_array
+
+
+def gaussFilt(im,filt_const):
+
+	im[:,:,0] = sf.gaussian_filter(im[:,:,0], filt_const)
+	im[:,:,1] = sf.gaussian_filter(im[:,:,1], filt_const)
+	im[:,:,2] = sf.gaussian_filter(im[:,:,2], filt_const)
+
+	return im
+
+def block_mean(im_array):
+
+	array_size=np.shape(im_array)[2]
+	mean_array=np.zeros((1,array_size))
+	for i in range(0,array_size):
+		mean_array[0,i]=im_array[:,:,i].mean()
+	mean_array/=np.amax(mean_array[:])
+	return mean_array
+
+def featureExt(tType):
+
+	num_features=0
+	div=256/levels
+	im_height="../images/divided images/vricon_dsm" + filename +".tif"
+
+	# Import the images
+	im_blue = cv2.imread("../images/divided images/vricon_ortho_blue" + filename +".png")
+	im_red = cv2.imread("../images/divided images/vricon_ortho_red" + filename +".png")
+	im_green = cv2.imread("../images/divided images/vricon_ortho_green" + filename +".png")
+	im_nir = cv2.imread("../images/divided images/vricon_ortho_nir" + filename +".png")
+	im_pan=cv2.imread("../images/divided images/vricon_ortho_pan" + filename +".png")
+
+	num_blocks=np.shape(im_blue)[0]**2/(N**2)
+
+	# Filtrating the image
+	inImageB = gaussFilt(im_blue,gaussNr)
+	inImageR = gaussFilt(im_red,gaussNr)
+	inImageG = gaussFilt(im_green,gaussNr)
+	inImageN = gaussFilt(im_nir,gaussNr)
+	inImageP = gaussFilt(im_pan,gaussNr)
+
+	#mean and graylevels
+	imageB = np.floor((inImageB[:,:,0]+inImageB[:,:,1]+inImageB[:,:,2])/(3.0*div))
+	imageR = np.floor((inImageR[:,:,0]+inImageR[:,:,1]+inImageR[:,:,2])/(3.0*div))
+	imageG = np.floor((inImageG[:,:,0]+inImageG[:,:,1]+inImageG[:,:,2])/(3.0*div))
+	imageN = np.floor((inImageN[:,:,0]+inImageN[:,:,1]+inImageN[:,:,2])/(3.0*div))
+	imageP = np.floor((inImageP[:,:,0]+inImageP[:,:,1]+inImageP[:,:,2])/(3.0*div))
+
+	#split images into arrays
+	splitB=utils.splitImage(imageB,N,num_blocks)
+	splitR=utils.splitImage(imageR,N,num_blocks)
+	splitG=utils.splitImage(imageG,N,num_blocks)
+	splitN=utils.splitImage(imageN,N,num_blocks)
+	splitP=utils.splitImage(imageP,N,num_blocks)
+
+	meanB=block_mean(splitB)
+	meanR=block_mean(splitR)
+	meanG=block_mean(splitG)
+	meanN=block_mean(splitN)
+	meanP=block_mean(splitP)
+
+	statsB=imStats(splitB,levels)
+	statsR=imStats(splitR,levels)
+	statsG=imStats(splitG,levels)
+	statsN=imStats(splitN,levels)
+	statsP=imStats(splitP,levels)
+
+	height=heightFeatures.heightFeatures(im_height,N,"none")
+
+	arrayImage=np.append(meanB,meanR,axis=0)
+	arrayImage=np.append(arrayImage,meanG,axis=0)
+	arrayImage=np.append(arrayImage,meanN,axis=0)
+	arrayImage=np.append(arrayImage,meanP,axis=0)
+	arrayImage=np.append(arrayImage,statsB,axis=0)
+	arrayImage=np.append(arrayImage,statsR,axis=0)
+	arrayImage=np.append(arrayImage,statsG,axis=0)
+	arrayImage=np.append(arrayImage,statsN,axis=0)
+	arrayImage=np.append(arrayImage,statsP,axis=0)
+	arrayImage=np.append(arrayImage,height,axis=0)
+
+	#Save data to file
+	num_features=np.shape(arrayImage)[0]
+	file_name="f"+str(num_features)+"_g"+str(levels)+"_b"+str(N)+"_gau"+str(gaussNr)+"_"+tType
+
+	np.save(file_name, arrayImage)
+
+	print "Complete"
+
+
+	
+#ANDRA ENDAST DESSA VARDEN, STRANGAR OCH SOKVAGAR FIXAR SIG SJALVA
+levels = 128 		#greyscale levels
+N = 4 				#blockssize
+gaussNr = 1			#gauss Sigma
+filename="3"
+
+
+featureExt(filename)
+#featureExt("train")
---
+++ Network/heightFeatures.py
@@ -0,0 +1,78 @@
+from PIL import Image
+import numpy as np
+import scipy as sp
+import cv2
+from osgeo import osr, gdal
+import skimage.io
+import scipy.ndimage
+from skimage import io
+from matplotlib import pyplot as plt
+
+
+def heightFeatures(image, block_size, divide):
+	im = skimage.io.imread(image, plugin='tifffile')
+	
+	threshold = 0 #Remove negative values (generally -1024)
+	idx = im[:,:] < threshold
+	im[idx] = 0
+
+	kernel_size = 101 #for threshold, trial and error to get good results
+
+	im_xsize = im.shape[0]
+	im_ysize = im.shape[1]
+	
+	if (divide == 'multiple'):
+		# GLOBAL VARIABLES
+		cutout_xmin2 = 5500
+		cutout_ymin2 = 1500
+		cutout_xsize = 2000
+		cutout_ysize = 2000
+		x_range = int(np.floor(im_xsize/cutout_xsize)*cutout_xsize)
+		y_range = int(np.floor(im_ysize/cutout_ysize)*cutout_ysize)
+
+		counter = 1
+		for j in range(0,y_range, int(cutout_ysize)):
+			cutout_xmin = j
+			for k in range(0,x_range, int(cutout_xsize)):
+				cutout_ymin = k
+				c = str(counter)
+				cutout = im[k:k+cutout_xsize,j:j+cutout_ysize]
+				cutout = cv2.convertScaleAbs(cutout)
+				cutout_thres = cv2.adaptiveThreshold(cutout,np.amax(cutout),cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,kernel_size,0)
+				cutout_thres = scipy.ndimage.zoom(cutout_thres, 1/float(N), order=0) #downsample image
+				array_length = cutout_thres.size
+				cutout_thres_array = np.reshape(cutout_thres,[1,array_length])
+				cutout_thres_array /= np.amax(cutout_thres_array)
+				np.save(str(output_filename + str(counter) + '.npy'), cutout_thres_array)
+				#cv2.imwrite(str(output_filename + str(counter) + '.png'), cutout_thres)
+				counter = counter +1
+				print counter
+	else:
+		if(divide=='one'): #if a cutout is to be made
+			im = im[cutout_xmin2:(cutout_xmin2+cutout_xsize), cutout_ymin2:(cutout_ymin2+cutout_ysize)]
+		# Threshold
+		im = cv2.convertScaleAbs(im)
+		im_thres = cv2.adaptiveThreshold(im,np.amax(im),cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,kernel_size,0)
+		im_thres = scipy.ndimage.zoom(im_thres, 1/float(block_size), order=0) #downsample image
+		np.set_printoptions(threshold = np.nan)
+		array_length = im_thres.size
+		im_thres_array = np.reshape(im_thres,[1,array_length])
+		im_thres_array /= np.amax(im_thres_array)
+		return im_thres_array
+
+
+		#np.save(str(output_filename + '.npy'), im_thres_array)
+		#plt.figure()
+		#io.imshow(im_thres)
+		#io.show()
+
+
+
+
+#N = 4 #blockssize #for std calc, same as other features
+
+#image = '0_1_0_dsm.tif' #input dsm image, must be tif
+#output_name = 'testKarin3_t' #output name, npy format
+
+# use 'none', 'one' or 'multple' as last argument
+#heightFeatures(image, output_name, 'none')
