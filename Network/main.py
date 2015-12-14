1d12df61efb210043347635ef6fa7c4b72c3e833
hanjarr <hanjarr@gmail.com>
2015-12-14 13:22:47 +0100

    Updates in main and utils

--- Network/main.py
+++ Network/main.py
@@ -13,32 +13,42 @@ import cv2
 def main():
 
 
-	'''Load osm images for training and test (validation)'''
+	''' Specify path to images'''
 	osm_path ="../images/divided images/rasterized"
 
 
-	'''Choose features for training and test'''
+	''' Specify path to features'''
 	feature_path = "../python features/feature_vectors/f26_g128_b4_"
 
-	im_numbers_train = [1,4,5,7,8,11,13,17,21,22,24,25,29,31,37,38,39,41,45,46,48,49,52,54,55,57,62]
+	''' Choose cutouts to use for training and testing'''
+	im_numbers_train = [1]#,4,5,7,8,11,13,17,21,22,24,25,29,31,37,38,39,41,45,46,48,49,52,54,55,57,62]
 	im_number_test = [18] #[2] [9] [16] [23] [30] 
 
 
-	'''Load original for blending'''
+	''' Load original image for blending'''
 	test_original = "../images/divided images/vricon_ortho_pan"+str(im_number_test[0])+".png"
 
 
-	'''Use new func to generate array elements'''
-	[training_osm_array, training_features_array] = inputNetworkArray(osm_path, feature_path, im_numbers_train)
+	''' Generate array elements'''
 	[test_osm_array, test_features_array] = inputNetworkArray(osm_path, feature_path, im_number_test)
+	[training_osm_array, training_features_array] = inputNetworkArray(osm_path, feature_path, im_numbers_train)
 
 
+	''' Create folder to save data in'''
+	dirfmt = "../saved/%4d-%02d-%02d-%02d-%02d-%02d"
+	save_dir = dirfmt % time.localtime()[0:6]
+	os.mkdir(save_dir)
+
+	''' Specify which net to load if you want to load an existing network'''
+	load_net = "../saved/2015-12-09-18-05-21/network"
+
+	''' Imread ground truth test image''' 
 	test_osm =cv2.imread(osm_path+str(im_number_test[0])+".png")[:,:,0]
 
-	'''Choose features for training and test'''
-
+	''' Choose features for training and test'''
 	info = map(int, re.findall(r'\d+', feature_path))
-
+	
+	''' Specify network parameters'''
 	block_dim = info[2]
 	input_layer = info[0]
 
@@ -46,26 +56,20 @@ def main():
 	hidden_layer_2 = 20
 	output_layer = 3
 
-	epochs = 100
+	epochs = 10
 	mini_batch = 10
 	learning_rate = 0.005
 
 	sizes = [input_layer,hidden_layer, hidden_layer_2, output_layer]
 
-	'''Create folder to save data in'''
-	dirfmt = "../saved/%4d-%02d-%02d-%02d-%02d-%02d"
-	save_dir = dirfmt % time.localtime()[0:6]
-	os.mkdir(save_dir)
-
-	'''Specify which net to load if you want to load an existing network'''
-	load_net = "../saved/2015-12-04-13-30-13/network"
-
+	'''Create utils object'''
 	utils = Utils(block_dim, input_layer, output_layer,training_osm_array, save_dir)
-	training_data, test_data = utils.loadData(training_osm_array, test_osm_array, training_features_array, test_features_array)
+
+	''' Load validation data'''
+	test_data = utils.loadData(test_osm_array, test_features_array)
 
 
-
-	'''Load existing network from folder svaved_network'''
+	''' Load existing network from folder saved_network if it is specified'''
 	if load_net:
 		net = network2.load(load_net)
 		total, accuracy, confusion = net.accuracy(test_data)
@@ -74,11 +78,15 @@ def main():
 	else:
 		net = Network(sizes)
 
+		''' Configure training data'''
+		training_data = utils.loadData(training_osm_array, training_features_array, training = True)
+
+		''' Train the network using SGD'''
 		evaluation_cost, evaluation_accuracy, training_cost, training_accuracy, test_confusion, data = \
 		net.SGD(training_data, epochs, mini_batch, learning_rate, save_dir,lmbda=0.5, evaluation_data = test_data, \
 		monitor_training_cost = True, monitor_evaluation_cost = True, monitor_training_accuracy=True, monitor_evaluation_accuracy=True)
 
-		''' Create accuracy plot'''
+		''' Create accuracy plots'''
 		if evaluation_accuracy:
 			utils.plotAccuracy(evaluation_accuracy, "/evaluation_accuracy")
 
@@ -93,13 +101,20 @@ def main():
 		if test_confusion.any():
 			utils.plotConfusionMatrix(test_confusion)
 
+		''' Load the best network after training for testing'''
 		net = network2.load(save_dir+'/network')
 
-	processed_result_resized, processed_stat_resized = utils.createImage(net, test_data, test_osm, test_original)
-	processed_result_accuracy = utils.evaluateResult(test_osm,processed_result_resized).tolist()
-	processed_stat_accuracy = utils.evaluateResult(test_osm,processed_stat_resized).tolist()
 
-	data.update({"evaluation result after process G for original output image": processed_result_accuracy, "evaluation result after process G for statistical image": processed_stat_accuracy,
+	''' Create images from the classifications'''
+	processed_result_resized, processed_stat_resized = utils.createImage(net, test_data, test_osm, test_original)
+
+	''' Calculate accuracy after processing '''
+	processed_result_accuracy = utils.evaluateResult(test_osm, processed_result_resized).tolist()
+	processed_stat_accuracy = utils.evaluateResult(test_osm, processed_stat_resized).tolist()
+
+	''' Update info file that is generated'''
+	data.update({"evaluation result after process G for original output image": processed_result_accuracy, 
+		"evaluation result after process G for statistical image": processed_stat_accuracy,
 		"test image": im_number_test, "training_images": im_numbers_train})
 
 	f = open(save_dir + "/info", "w")
